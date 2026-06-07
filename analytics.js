1f69af4fa4798bf91536a8a8247204c896bca464
matt-east <mseast@icloud.com>
2026-06-07 16:11:29 -0500

    Revert "Install and configure Vercel Web Analytics"

--- analytics.js
+++ analytics.js
@@ -1,7 +1,3 @@
 import { inject } from '@vercel/analytics';
 
-// Initialize Vercel Web Analytics
-// Documentation: https://vercel.com/docs/analytics/quickstart
-inject({
-  mode: 'auto', // Automatically detect environment (development/production)
-});
+inject();
